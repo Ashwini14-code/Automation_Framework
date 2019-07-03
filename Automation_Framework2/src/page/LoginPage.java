332c3f7a30c0758ce42ed7248908911067ddab93
Ashwini14-code <52453462+Ashwini14-code@users.noreply.github.com>
2019-07-03 20:15:25 +0530

    Updated Loginpage class

---
+++ Automation_Framework2/src/page/LoginPage.java
@@ -0,0 +1,45 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import junit.framework.Assert;
+
+public class LoginPage 
+{
+  @FindBy(id="username")
+  private WebElement unTB;
+  @FindBy(name="pwd")
+  private WebElement pwTB;
+  @FindBy(xpath="//div[.='Login']")
+  private WebElement loginBTN;
+  @FindBy(xpath="//span[contains(.,'invalid')]")
+  private WebElement errorMSG;
+  
+  public LoginPage(WebDriver driver)
+  {
+	  PageFactory.initElements(driver,this);
+  }
+  public void setuserName(String un)
+  {
+	  unTB.sendKeys(un);
+  }
+  public void setPassword(String pw)
+  {
+	  pwTB.sendKeys(pw);
+  }
+  public void clickLogin()
+  {
+	  loginBTN.click();
+  }
+  public void verifyErrorMSG(String expectedMSG)
+  {
+	  String actualMSG = errorMSG.getText();
+	  Assert.assertEquals(actualMSG,expectedMSG);
+  }
+  
+  
+  
+}
